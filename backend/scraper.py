9ad68ba5da08dabcb56bec6bfcce138c397b846c
JaspDev <162011787+JaspDev@users.noreply.github.com>
2025-10-13 17:17:20 -0700

    (linking git repo w/ vscode, pay no mind to this) - Jason

--- backend/scraper.py
+++ backend/scraper.py
@@ -2,3 +2,5 @@
 # Main job is to fetch menu data from UCSC dining website and save it in a structured JSON format
 # This data is then served by the Flask app in app.py
 # Need to implement automatic updates (e.g. via cron job) to keep data fresh
+
+# This is a test to see if my VS-code linked correctly with the git repository pay no mind to this - Jason
