2f5a0c26ac00d81620e514d7030182efa03dca06
jwglyph <wujacky1369@gmail.com>
2025-10-18 18:25:02 -0700

    fixed website access error by navigating from homepage instead of direct menu links

--- backend/scraper.py
+++ backend/scraper.py
@@ -1,41 +1,41 @@
-#Scrapes UCSC dining data and updates JSON file
-# Main job is to fetch menu data from UCSC dining website and save it in a structured JSON format
-# This data is then served by the Flask app in app.py
-# Need to implement automatic updates (e.g. via cron job) to keep data fresh
-
-#=======// UCSC MENU LINKS //=======#
-#  John R. Lewis & Colleger Nine:   https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum=40&locationName=John+R.+Lewis+%26+College+Nine+Dining+Hall&naFlag=1
-#  Cowell & Stevenson:   https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum=05&locationName=Cowell+%26+Stevenson+Dining+Hall&naFlag=1
-#  Crown & Merrill and Banana Joe's:   https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum=20&locationName=Crown+%26+Merrill+Dining+Hall+and+Banana+Joe%27s&naFlag=1
-#  Porter & Kresge:   https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum=25&locationName=Porter+%26+Kresge+Dining+Hall&naFlag=1
-#  Rachel Carson & Oakes:   https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum=30&locationName=Rachel+Carson+%26+Oakes+Dining+Hall&naFlag=1
-#=======// UCSC MENU LINKS //=======#
-
-#source = requests.get('https://nutrition.sa.ucsc.edu/').text 
-#soup = BeautifulSoup(source, 'html.parser')  ## stores the html in soup
-# print(soup.prettify())
-#all_menus = soup.find_all('a', limit = 5)  ## We only need the first 5 menus, the rest are the stores & cafes
-#menus = {}  ## key is location and val is the link to menu
-#for m in all_menus:
-#    menus[m.get_text(strip=True)] = 'https://nutrition.sa.ucsc.edu/' + m['href']
-#for (key, val) in menus.items():
-#    print('College Name:', key)
-#    print('Link to Menu:', val, '\n')
-
-from bs4 import BeautifulSoup
-import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import time
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome()
-url = "https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum=25&locationName=Porter+%26+Kresge+Dining+Hall&naFlag=1"
+options = Options()
+# options.add_argument("--headless=new") # comment for visual testing of functionality
+options.add_argument("--start-maximized")
+options.add_argument("--user-agent=Mozilla/5.0")
 
-driver.get(url)
-time.sleep(5)
-soup2 = BeautifulSoup(driver.page_source, 'html.parser')
+driver = webdriver.Chrome(options=options)
+driver.get("https://nutrition.sa.ucsc.edu/")
 
-driver.quit()
+try:
+    # Wait for the dining hall links to appear
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR, "li.locations a"))
+    )
 
-print(soup2) #Gives the ASP.NET runtime error in the form of html
+    # Find and click the “Slug Stop” link or whichever place we need menu data from
+    for a in driver.find_elements(By.CSS_SELECTOR, "li.locations a"):
+        if "Slug Stop" in a.text:
+            a.click()
+            break
+
+    # Wait for menu items to load
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.shortmenurecipes span"))
+    )
+
+    # Collect and print item names
+    items = [e.text.strip() for e in driver.find_elements(By.CSS_SELECTOR, "div.shortmenurecipes span") if e.text.strip()]
+    print(" | ".join(items))
+
+finally:
+    driver.quit()
+
+# Previous issue was cuased by Selenium using direct links to dining locations,
+# which failed due to missing session cookies. Resolved by first loading the
+# homepage to establish cookies before navigating to the deseried dining menu.
